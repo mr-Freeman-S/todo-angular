2d0e135a84852ff38249875e3f358562b670ff3d
Slava <mr.baranok.slava@gmail.com>
2022-12-02 23:17:59 +0300

    add input

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoInputComponent } from './components/todo-input/todo-input.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoInputComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/todo-input/todo-input.component.ts
+++ src/app/components/todo-input/todo-input.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-todo-input',
   templateUrl: './todo-input.component.html',
   styleUrls: ['./todo-input.component.scss']
 })
-export class TodoInputComponent {
+export class TodoInputComponent implements OnInit{
+  todoTitle:string = ''
+  constructor() {
+  }
+  ngOnInit():void {
+  }
 
+  submitTodo(title:string) {
+    this.todoTitle = title
+
+  }
 }
