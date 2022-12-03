83d759fdb0f533515a3146f32bb2706a6c0e788c
Slava <mr.baranok.slava@gmail.com>
2022-12-03 23:54:08 +0300

    add submit emit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {TodoModel} from "./models/todo-model";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-angular-app';
+  todoItems: TodoModel[] = [
+    {title: 'angular introducing'},
+    {title: 'interface introducing'},
+  ];
+
+  constructor() {
+  }
+
+  addTodo(titleTodo: string) {
+
+  }
+
 }
--- src/app/components/todo-input/todo-input.component.ts
+++ src/app/components/todo-input/todo-input.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-todo-input',
@@ -9,11 +9,11 @@ export class TodoInputComponent implements OnInit{
   todoTitle:string = ''
   constructor() {
   }
+  @Output() submit = new EventEmitter<string>()
   ngOnInit():void {
   }
 
-  submitTodo(title:string) {
-    this.todoTitle = title
-
+  submitTodo() {
+    this.submit.emit(this.todoTitle)
   }
 }
